4a05bb3c7f74dd4ddf9684ae7d3fdc3ee02ca69e
[NaomiS-ara] <[Naomispencer101@gmail.com]>
2025-10-31 14:08:47 -0400

    HW ASSIGN

--- src/from-scratch.js
+++ src/from-scratch.js
@@ -1,11 +1,28 @@
 // find
-const getUserById = () => { };
+const getUserById = (users, id) => { };
+ // 'users' is an array of user objects
+  // 'id' is the number we’re looking for
+return users.find(user => user.id === id)
+// go through each 'user' in the 'users' array
+  // check if user.id equals the 'id' we passed in
+  // if it matches, return that user object
+  // if no match, return undefined automatically
+
+//arrow function version =
+//const getUserById = (users, id) => users.find(user => user.id === id);
+
 
 // findIndex
-const getIndexOfApple = () => { };
+const getIndexOfApple = (index) => { };
+return foods.findIndex(food => food.name === "apple");
+
 
 // reduce
 const getSquaredTotal = () => { };
+getSquaredTotal([1, 2, 3]);
+// → 14 (1*1 + 2*2 + 3*3)
+return getSquaredTotal.reduce(Squared => Squared.reduce === "");
+
 
 // every
 const hasOnlyEvenNumbers = () => { };
